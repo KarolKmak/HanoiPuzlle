03956744889eee0dbca81fb43e1cba33dcb36bd3
kar.kmak <00grunt@gmail.com>
2023-06-15 12:59:36 +0200

    fixing getState() function

--- app/src/main/java/com/example/hanoipuzlle/GameLogic.kt
+++ app/src/main/java/com/example/hanoipuzlle/GameLogic.kt
@@ -2,35 +2,34 @@ package com.example.hanoipuzlle
 
 class GameLogic {
 
-    //Im niższa pozycja tym wyższy punkt wieży
+    //Im niższa pozycja tym wyższy punkt wieży 0 -szczyt, ostani element - dół wieży
     private var gameState = mutableListOf<Int>(0,0,0,0)
 
     //Odczytanie wartości poziomu najwyższego elementu wybranej wieży
     fun getState (tower: Int): Int {
         for (i in 0 until gameState.size) {
             if (gameState[i] == tower) {
-                //zwrócenie numeru segmentu
+                //zwrócenie wysokości segmentu
                 return i
-                break
             }
         }
-        //zwrócenie niewłaściwego numeru wieży - nie ma żadnego elementu w wybranej wieży
-        return 3
+        //zwrócenie wartości -1 - brak segmentów w danej wieży
+        return -1
     }
 
-    //Sprawdzenie czy można przesunąć segment na określoną pozycję
-    fun canChange(place: Int,value: Int): Boolean {
-        for (i in 0 until place){
-            if(gameState[i] == value){
+    //Sprawdzenie czy można przesunąć segment na określoną wieżę
+    fun canChange(segment: Int,tower: Int): Boolean {
+        for (i in 0 until segment){
+            if(gameState[i] == tower){
                 return false
             }
         }
         return true
     }
 
-    //Zmiana pozycji segmentu wieży
-    fun changeState(place: Int, value: Int) {
-        gameState[place] = value
+    //Zmiana pozycji segmentu wieży; użyć canChange() wcześniej by sprawdzić czy ruch jest dopuszczalny
+    fun changeState(segment: Int, tower: Int) {
+        gameState[segment] = tower
     }
 
     //ToDo
